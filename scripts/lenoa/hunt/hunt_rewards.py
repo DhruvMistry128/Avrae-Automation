22fa612f105e4d3b9b5f9e453e3d471300df150d
DhruvMistry128 <dhruvmistry128@gmail.com>
2026-02-26 09:58:06 -0600

    Remove redundant type checks for string validation in hunt_rewards.py

--- scripts/lenoa/hunt/hunt_rewards.py
+++ scripts/lenoa/hunt/hunt_rewards.py
@@ -32,19 +32,11 @@ if args.get('name') is None:
     desc += f'{t.error["missing_args"]} (-name) args expected'
     base += f' -desc "{desc}"'
     return base
-if typeof(args.get('name')) is not str:
-    desc += f'{t.error["invalid_type"]} (-name) args must be a string'
-    base += f' -desc "{desc}"'
-    return base
 nameArg=args.last('name')
 desc += f'**Adventure**: {nameArg}\n'
 
 if ctx.author.id is None:
     desc += f'{t.error["author"]}'
-    base += f' -desc "{desc}"'
-    return base
-if typeof(ctx.author.id) is not str:
-    desc += f'{t.error["invalid_type"]} Author ID must be a string'
     base += f' -desc "{desc}"'
     return base
 dmNameArg='<@'+ctx.author.id+'>'
@@ -138,10 +130,6 @@ base += f' {fParty}'
 if args.get('dm') is None:
     error = t.error["missing_args"] + "(-dm) args expected"
     base += f' -f "{error}"'
-    return base
-if typeof(args.get('dm')) is not str:
-    desc += f'{t.error["invalid_type"]} (-dm) args must be a string'
-    base += f' -f "{desc}"'
     return base
 
 dmArg=args.last('dm')
